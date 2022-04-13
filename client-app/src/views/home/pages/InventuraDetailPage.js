294e3e0861714a1c6ad695f860073b5d80edd8e7
Petr Jílek <petr.jilek@netglade.cz>
2022-04-13 14:30:37 +0200

    loading addedItems

--- client-app/src/views/home/pages/InventuraDetailPage.js
+++ client-app/src/views/home/pages/InventuraDetailPage.js
@@ -14,19 +14,28 @@ export default function InventuraDetailPage() {
 
     const [eanItems, setEanItems] = useState([])
 
+    const [addedEanItems, setAddedEanItems] = useState([])
+
     useEffect(() => {
-        get();
+        get()
     }, []);
 
     const get = async () => {
         var data = await axios.get(`firma1/inventura/${id}`);
         setItem(data.winstrom.inventura[0])
+
+        var addedEans = await axios.get(`https://inventura.flexibee.eu/v2/c/firma1/inventura-polozka/%28inventura%3D${data.winstrom.inventura[0].id}%29?detail=custom%3Aid%2Csklad%2CdatZahaj%2CmnozMjReal%2Ccenik&limit=100&offset=0`)
+        setAddedEanItems(addedEans.winstrom["inventura-polozka"])
     }
 
     const sendEan = async () => {
-        console.log(ean)
-
-        var data = await axios.get(`firma1/skladova-karta/%28sklad%20%3D%20%22code%3ASKLAD%22%20and%20ucetObdobi%20%3D%20%22code%3A2022%22%20and%20cenik%3D%22ean%3A${ean}%22%29?detail=custom%3Aid%2Csklad%2CdatZahaj%2Ccenik&limit=10&offset=0`)
+        /*
+         var d = await axios.get(`https://inventura.flexibee.eu/v2/c/firma1/skladova-karta/%28sklad%20%3D%20%22code%3ASKLAD%22%20and%20ucetObdobi%20%3D%20%22code%3A2022%22%20and%20cenik%3D%22ean%3A${ean}%22%29?detail=custom%3Acenik%28eanKod%2Cnazev%2Ckod%29%2Csklad%2Cobdobi%2CstavMj%26includes%3D%2Fskladova-karta%2Fcenik&limit=10&offset=0`)
+ 
+         console.log(d);
+ 
+         */
+        var data = await axios.get(`firma1/skladova-karta/%28sklad%20%3D%20%22${item.sklad}%22%20and%20ucetObdobi%20%3D%20%22code%3A2022%22%20and%20cenik%3D%22ean%3A${ean}%22%29?detail=custom%3Aid%2Csklad%2CdatZahaj%2Ccenik&limit=10&offset=0`)
 
         var eItems = data.winstrom["skladova-karta"]
 
@@ -68,12 +77,30 @@ export default function InventuraDetailPage() {
 
 
     const saveEanItems = async () => {
-        console.log(eanItems)
+        var data = {
+            winstrom: {
+                "inventura-polozka": []
+            }
+        }
+
+        eanItems.forEach(x =>
+            data.winstrom["inventura-polozka"].push({
+                inventura: item.id,
+                sklad: item.sklad,
+                cenik: x.id,
+                skladKarta: x.id,
+                mnozMjReal: x.mycount
+            })
+        )
+
+        await axios.post("https://inventura.flexibee.eu/v2/c/firma1/inventura-polozka", data)
+
+        await get()
     }
 
     return (
         <>
-            <p>InventuraDetailPage</p>
+            <h1>Detail Inventury</h1>
             <h3>Id</h3>
             <p>{item.id}</p>
             <h3>Typ Inventury</h3>
@@ -84,9 +111,27 @@ export default function InventuraDetailPage() {
             <p>{item.stavK}</p>
             <h3>sklad</h3>
             <p>{item.sklad}</p>
-
             <hr />
 
+            <h2>Sečtené položky</h2>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Nazev</th>
+                        <th>Pocet</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {addedEanItems.map(x =>
+                        <tr>
+                            <td>{x.cenik.split(":")[1]}</td>
+                            <td>{x.mnozMjReal}</td>
+                        </tr >)}
+                </tbody>
+            </Table>
+            <hr />
+
+            <h2>Načíst položky</h2>
             <Form.Group className="mb-3">
                 <Form.Label>EAN</Form.Label>
                 <Form.Control onChange={(e) => { setEan(e.target.value) }} />
