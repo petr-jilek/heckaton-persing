0dc17879a3dfa75d460a392ca4499168bb61c890
vetchmat <vetchmat@fel.cvut.cz>
2022-04-13 11:05:43 +0200

    forms

--- client-app/src/views/home/pages/HomePage.js
+++ client-app/src/views/home/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Form, form} from 'react-bootstrap'
+import { Button, Form, input} from 'react-bootstrap'
 import axios from 'axios'
 
 export default function HomePage() {
@@ -21,23 +21,30 @@ export default function HomePage() {
             <Button variant="primary" onClick={get}>Get</Button>
            <Button variant="warning" onClick={tryGet}>Try</Button>*/}
             
-            <div>
-            <Form.Label htmlFor="inputfirm_name">Enter name of starage</Form.Label>
+            <Form.Group className="mb-3">
+                <Form.Label>Enteer your username</Form.Label>
+                <Form.Control placeholder="Input Name" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Enteer your password</Form.Label>
+                <Form.Control placeholder="Input Password" />
+            </Form.Group>
+
+            <Form.Label htmlFor="inputfirm_name">Enter name of storage</Form.Label>
            {/* <Form.Control
                 type="storage_name"
                 id="inputstorage_name"
             />*/}
-            </div>
             <div></div>
             <select id="storage_name" name="storage_name">
             <option value="Storage1">Storage1</option>
             <option value="Storage2">Storage2</option>
             </select>
             <div>
-            <Button onclick="window.location.href='https://www.google.com/';">
+            <Button onclick="window.location.href='';">
                 Continue
                 </Button>
-                </div>
+            </div>
         </>
     );
 }
