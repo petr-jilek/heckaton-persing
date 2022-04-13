c48c4f9df278db330397c0ba1f309e7b82db77ab
vetchmat <vetchmat@fel.cvut.cz>
2022-04-13 10:40:24 +0200

    update

--- client-app/src/views/home/pages/HomePage.js
+++ client-app/src/views/home/pages/HomePage.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Button } from 'react-bootstrap'
+import { Button, Form, form} from 'react-bootstrap'
 import axios from 'axios'
 
 export default function HomePage() {
-
+/*
     const get = async () => {
         var data = await axios.get("/WeatherForecast");
         console.log(data);
@@ -13,12 +13,31 @@ export default function HomePage() {
         var data = await axios.get("/WeatherForecast/try");
         console.log(data);
     }
-
+*/
     return (
         <>
-            <p>HomePage</p>
+            
+             {/*<p>HomePage</p>
             <Button variant="primary" onClick={get}>Get</Button>
-            <Button variant="warning" onClick={tryGet}>Try</Button>
+           <Button variant="warning" onClick={tryGet}>Try</Button>*/}
+            
+            <div>
+            <Form.Label htmlFor="inputfirm_name">Enter name of starage</Form.Label>
+           {/* <Form.Control
+                type="storage_name"
+                id="inputstorage_name"
+            />*/}
+            </div>
+            <div></div>
+            <select id="storage_name" name="storage_name">
+            <option value="Storage1">Storage1</option>
+            <option value="Storage2">Storage2</option>
+            </select>
+            <div>
+            <Button onclick="window.location.href='https://www.google.com/';">
+                Continue
+                </Button>
+                </div>
         </>
     );
 }
