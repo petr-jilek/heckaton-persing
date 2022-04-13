ffd14860fd8bf4b9df438068360bd702a16f80fa
Petr Jílek <petr.jilek@netglade.cz>
2022-04-13 11:15:28 +0200

    aafsfdghj

--- client-app/src/views/home/pages/HomePage.js
+++ client-app/src/views/home/pages/HomePage.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import { Button, Form, input} from 'react-bootstrap'
+import { Button, Form, input } from 'react-bootstrap'
 import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
-/*
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        get();
+    }, []);
+
     const get = async () => {
-        var data = await axios.get("/WeatherForecast");
-        console.log(data);
+        var data = await axios.get("firma1/sklad?detail=full&limit=10&offset=0");
+        setItems(data.winstrom.sklad)
     }
 
-    const tryGet = async () => {
-        var data = await axios.get("/WeatherForecast/try");
-        console.log(data);
-    }
-*/
     return (
         <>
-            
-             {/*<p>HomePage</p>
+            {items.map(x => <p>{x.nazev}</p>)}
+
+            {/*<p>HomePage</p>
             <Button variant="primary" onClick={get}>Get</Button>
            <Button variant="warning" onClick={tryGet}>Try</Button>*/}
-            
+
             <Form.Group className="mb-3">
                 <Form.Label>Enteer your username</Form.Label>
                 <Form.Control placeholder="Input Name" />
@@ -31,18 +33,18 @@ export default function HomePage() {
             </Form.Group>
 
             <Form.Label htmlFor="inputfirm_name">Enter name of storage</Form.Label>
-           {/* <Form.Control
+            {/* <Form.Control
                 type="storage_name"
                 id="inputstorage_name"
             />*/}
             <div></div>
             <select id="storage_name" name="storage_name">
-            <option value="Storage1">Storage1</option>
-            <option value="Storage2">Storage2</option>
+                <option value="Storage1">Storage1</option>
+                <option value="Storage2">Storage2</option>
             </select>
             <div>
-            <Button onclick="window.location.href='';">
-                Continue
+                <Button onclick="window.location.href='';">
+                    Continue
                 </Button>
             </div>
         </>
